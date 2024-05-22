424322b3a2694a05c110f5ac0ea396f6bfafeca7
willingtonkutar <willykutar@gmail.com>
2024-05-22 16:30:30 +0300

    done

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sL "$1"
+# Sends a request to the URL and displays the size of the body of the response in bytes
+curl -s "$1" | wc -c
