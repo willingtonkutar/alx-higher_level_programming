18bf8ddfa3595b0fe087f5201aab14ae3f312788
willingtonkutar <willykutar@gmail.com>
2024-06-08 00:03:57 +0300

    newest update

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as file:
+    response = file.read()
+    print("Body response:")
+    print(f"\t- type: {type(response)}")
+    print(f"\t- content: {response}")
+    print(f"\t- utf8 content: {response.decode('utf-8')}")
